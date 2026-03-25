8ea028043d256eb671dbc8f511ae60a114f0d476
Anandhbasha <anadhakm@gmail.com>
2026-03-25 09:06:30 +0530

    hoisting

---
+++ Js/main.js
@@ -0,0 +1,44 @@
+// let h1 = document.createElement("h1")
+// h1.innerHTML = "Hello Welcome"
+// document.body.append(h1)
+
+// alert("Hello")
+
+// console.log("Hello");
+
+// Hoisting
+//variable declaration
+// var
+// var a=10
+//a-> variable
+//=-> assignment opeartor
+//10-> value
+// console.log(b);
+// a=20 //re assign
+// var b=a
+//type is var
+// b=30
+// var b= 60 //re declare
+// console.log(b);
+// Es6 concept ->Ecma Script
+// let
+// console.log(a);
+// let a =20
+// console.log(a);
+//const
+// const b = 30
+// b=50
+// console.log(b);
+
+// var 
+// global scope
+// let,const
+// local scope
+let a =20
+{
+    //string concordination
+    console.log("The a value is:"+a);  
+}
+
+
+
