e49fdf76eb5dd1e5c682afca645bb55de2a49b12
Anandhakumar <anandhaanavamani@gmail.com>
2026-06-05 21:23:35 +0530

    py

---
+++ Python/index.py
@@ -0,0 +1,78 @@
+# print("Hello welcome" ,end="-")
+# # new line
+# print("This is Basics of Python")
+
+# print("Hello welcome\t","\tThis is Basics of Python",sep="&")
+
+a,b,c = 10,30,40
+
+# a,b,c = c,a,b
+# print(a,b,c)
+
+temp = a
+# a=b
+# b=c
+# c=a
+# print(a,b,c)
+# # datatype
+# simple data types
+# # int
+# # float
+# x=10.4
+# print(type(x))
+
+# # string
+# userName = "Jeeva"
+# print(type(userName))
+# # boolean
+
+# # complex datatypes
+# # # list
+# # lsist =  [10,20,30,40]
+# # lsist[2] =60
+# # print(lsist[0])
+# # # array is called as list
+# # # tuple
+# # t = (10,20,30,40)
+# # print(t[0])
+# # set
+# s = {10,20,30,40,10,20,30,40}
+# print(s)
+# # dictionary
+# person = {
+#     "name":"arun",
+#     "age":20,
+#     "city":"Cbe"
+# }
+
+# print(person["name"])
+
+# # slicing
+# # [0:]
+# # print(userName[::-1])
+# # start index:next point to end
+# # print(userName[-1])
+# # print(userName[::3])
+# # jeeva
+# # aveeJ
+
+
+# # userName = "Jeeva"
+# # userName[1] = "i"
+# # print(userName)
+# list1 = ["apple","orange","kiwi"]
+# # membership
+# print("Apple" not in list1)
+# # ASCII
+# # A=65
+# # indentity
+# a=10
+# b=10
+# print(a is b)
+
+# list1 = [1,2,3]
+# list2 = list1
+# print(list1 is list2)
+# ternary
+age = 18
+print("Eligible" if age>=18 else "not eligible")
